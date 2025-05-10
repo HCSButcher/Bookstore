8b6f1d784a622051562c55cf0bb7ce9ecf85042f
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-05-10 17:44:27 +0300

    third commit

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -40,6 +40,6 @@ userSchema.methods.comparePasswords = async function (userPassword) {
   return await bcrypt.compare(userPassword, this.password);
 };
 
-const User = mongoose.model("User, userSchema");
+const User = mongoose.model("User", userSchema);
 
 export default User;
