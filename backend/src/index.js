9c6ca88da818fee04386bf03a6d6c88a1fa44551
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-05-28 19:27:37 +0300

    6th commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,14 @@ import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
+import job from "./lib/cron.js";
 
 import { connectDB } from "./lib/db.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
