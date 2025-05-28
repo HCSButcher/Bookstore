95bbf6af8a21d868a68ece210dedea3ae4540516
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-05-28 20:49:16 +0300

    8th commit

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,9 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { useAuthStore } from "../store/authStore";
+import { useEffect } from "react";
 export default function Index() {
+  const { user, token, checkAuth } = useAuthStore();
+  console.log(user, token);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Hello {user?.username} </Text>
+      <Text style={styles.title}>Token {token} </Text>
 
       <Link href="/(auth)/signup">Signup</Link>
       <Link href="/(auth)">Login</Link>
