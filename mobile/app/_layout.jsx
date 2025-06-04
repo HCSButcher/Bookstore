2a6b26ef245da97382bc8877679746cead30d205
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-06-04 12:28:34 +0300

    11th commit

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,4 +1,4 @@
-import { Stack, useRouter, useSegments } from "expo-router";
+import { Slot, Stack, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
@@ -15,16 +15,19 @@ export default function RootLayout() {
     checkAuth();
   }, []);
 
-  //handle navigation based
-
-  useEffect(() => {});
-
+  //handle navigation based on the auth state
+  useEffect(() => {
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = user && token;
+    if (!isSignedIn && !inAuthScreen) router.replace("/(auth)");
+    else if (isSignedIn && inAuthScreen) router.replace("/(tabs)");
+  }, [user, token, segments]);
   return (
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="(auth)" />
         </Stack>
       </SafeScreen>
       <StatusBar style="dark" />
