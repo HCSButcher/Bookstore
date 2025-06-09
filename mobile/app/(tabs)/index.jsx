ed5b1b7dce36d797b43e3dd6494e72ffec263a52
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-06-09 20:35:26 +0300

    17th commit

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,7 +1,12 @@
 import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import { useAuthStore } from "../../store/authStore";
+import { Image } from "expo-image";
+import { API_URL } from "../../constants/api";
 import { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import styles from "../../assets/styles/home.styles";
+import COLORS from "../../constants/colors";
+import { formatPublishDate } from "../../lib/utils";
 
 export default function Home() {
   const { token } = useAuthStore();
@@ -11,7 +16,30 @@ export default function Home() {
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
 
-  const fetchBooks = async (pageNum = 1, refresh = false) => {};
+  const fetchBooks = async (pageNum = 1, refresh = false) => {
+    try {
+      if (refresh) setRefreshing(true);
+      else if (pageNum === 1) setLoading(true);
+
+      const response = await fetch(`${API_URL}/books?page=${pageNum}&limit=5`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      const data = await response.json();
+      if (!response.ok)
+        throw new Error(data.message || "Failed to fetch books");
+
+      setBooks((prevBooks) => [...prevBooks, ...data.books]);
+
+      setHasMore(pageNum < data.totalPages);
+      setPage(pageNum);
+    } catch (error) {
+      console.log("Error fetching Books", error);
+    } finally {
+      if (refresh) setRefreshing(false);
+      else setLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchBooks();
@@ -21,15 +49,51 @@ export default function Home() {
 
   const renderItem = ({ item }) => (
     <View style={styles.bookCard}>
-      <View style={styles.bookCard}>
-        <View style={styles.bookHeader}>
-          <View style={styles.userInfo}>
-            <Image source = {{uri:}} />
-          </View>
+      <View style={styles.bookHeader}>
+        <View style={styles.userInfo}>
+          <Image
+            source={{ uri: item.user.profileImage }}
+            style={styles.avatar}
+          />
+          <Text style={styles.username}>{item.user.username} </Text>
         </View>
+        <View style={styles.bookImageContainer}>
+          <Image
+            source={item.image}
+            style={styles.bookImage}
+            contentFit="cover"
+          />
+        </View>
+      </View>
+      <View style={styles.bookDetails}>
+        <Text style={styles.bookTitle}> {item.title} </Text>
+        <View style={styles.ratingContainer}>
+          {renderRatingStars(item.rating)}
+        </View>
+        <Text style={styles.caption}>{item.caption} </Text>
+        <Text style={styles.date}>
+          shared on {formatPublishDate(item.createdAt)}
+        </Text>
       </View>
     </View>
   );
+
+  //render stars
+  const renderRatingStars = (rating) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <Ionicons
+          key={i}
+          name={i <= rating ? "star" : "star-outline"}
+          size={16}
+          color={i <= rating ? "#f4b400" : "COLORS.textSecondary"}
+          style={{ marginRight: 2 }}
+        />
+      );
+    }
+    return stars;
+  };
 
   return (
     <View style={styles.container}>
